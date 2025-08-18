0e7f71986eeeaa865dd941250fcb49a0de8eaa23
Hafize Nur Sarı <hafizenursari26@gmail.com>
2025-08-18 22:43:31 +0300

    Profile page css edits

--- Login_MeetingSystem/LoginMeetingSystem/obj/Debug/net7.0/LoginMeetingSystem.AssemblyInfo.cs
+++ Login_MeetingSystem/LoginMeetingSystem/obj/Debug/net7.0/LoginMeetingSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoginMeetingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f73636b4fbe0fae44d2d0d623a5db028d46db5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+996585e6d07fdfc90ca45810169b01e055275027")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoginMeetingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoginMeetingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
